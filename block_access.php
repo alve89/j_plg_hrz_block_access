7cebcd52bf3b9d2fd6055477d112aed699c3cc1d
Stefan Herzog <devel@stefan-herzog.com>
2021-08-23 12:13:06 +0200

    Adding v1.1.4

--- block_access.php
+++ block_access.php
@@ -10,76 +10,72 @@ defined('_JEXEC') or die('Restricted access');
 jimport('joomla.plugin.plugin');
 jimport('joomla.error.exception');
 
+use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Application\CMSApplication;
 
 class plgSystemBlock_access extends JPlugin
 {
 
-	
-
-	
 	function plgSystemBlock_access(& $subject, $config)
 	{
 		parent::__construct($subject, $config);
 	}
 
-	
+
 	function onAfterInitialise() // onAfterDispatch()
 	{
-		$app	 	= JFactory::getApplication();
-		$user 		= JFactory::getUser();
-		$session	= JFactory::getSession();
-		
+		$app		= JFactory::getApplication();
+		$user 	= JFactory::getUser();
+		$session= JFactory::getSession();
+
 		if (!$this->params->get('securitykey') || $session->get('block_access'))
-		{	
+		{
 			return;
 		}
 		// Check if security key has been entered
 		$logged = isset($_GET[$this->params->get('securitykey')]);
-		
+
 		// Check the current area the user wants so enter (site / admin)
-		if ($app->isSite())
+		if ($app->isClient('site'))
 		{
 			$area = "site";
 		}
-		
-		if ($app->isAdmin())
+
+		if ($app->isClient('administrator'))
 		{
 			$area = "admin";
 		}
-		
+
 		$securedArea = strtolower($this->params->get('area'));
-		
+
 		if($area == $securedArea || $securedArea == "both")
 		{
-			$this->blockArea($logged);			
+			if ($logged)
+			{
+				$session = JFactory::getSession();
+				$session->set('block_access', true);
+	//			return true;
+			}
+			else {
+				$this->blockArea();
+			}
+
 		}
-	
+
 	}
-	
-	function blockArea($logged)
+
+	function blockArea()
 	{
-		$session = JFactory::getSession();
-		
-		if (!$logged)
+		if($this->params->get('typeOfBlock') == "message")
 		{
-			if($this->params->get('typeOfBlock') == "message")
-			{
-				header('HTTP/1.0 403 Forbidden');
-				die($this->params->get('text'));
-			}
-			elseif($this->params->get('typeOfBlock') == "errorpage")
-			{
-				header("HTTP/1.0 404 Not Found");
-				header("Location:".JUri::root()."404");
-			}
-
-
+			header('HTTP/1.0 403 Forbidden');
+			die($this->params->get('text'));
 		}
-		
-		if ($logged)
+		elseif($this->params->get('typeOfBlock') == "errorpage")
 		{
-			$session->set('block_access', true);
-//			return true;
+			$uri = Uri::getInstance();
+			$url = $uri->toString();
+			(CMSApplication::getInstance('site'))->redirect(JUri::root(), 301);
 		}
 	}
 }
