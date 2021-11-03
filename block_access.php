65b7252d3229db759b571c08884c81bfbddea33f
alve89 <devel@stefan-herzog.com>
2021-11-03 11:17:59 +0100

    Improving / enhancing redirect options

--- block_access.php
+++ block_access.php
@@ -15,6 +15,8 @@ use Joomla\CMS\Application\CMSApplication;
 
 class plgSystemBlock_access extends JPlugin {
 
+	private $securedArea = '';
+
 	function onAfterInitialise() { // onAfterDispatch()
 		$app		= JFactory::getApplication();
 		$user 	= JFactory::getUser();
@@ -24,21 +26,20 @@ class plgSystemBlock_access extends JPlugin {
 			return;
 		}
 		// Check if security key has been entered
-		$logged = isset($_GET[$this->params->get('securitykey')]);
+		$correctKey = isset($_GET[$this->params->get('securitykey')]);
 
 		// Check the current area the user wants so enter (site / admin)
 		if ($app->isClient('site')) {
 			$area = "site";
 		}
-
 		if ($app->isClient('administrator')) {
 			$area = "admin";
 		}
 
-		$securedArea = strtolower($this->params->get('area'));
+		$this->securedArea = strtolower($this->params->get('area'));
 
-		if($area == $securedArea || $securedArea == "both") {
-			if ($logged) {
+		if($area == $this->securedArea || $this->securedArea == "all") {
+			if($correctKey) {
 				$session = JFactory::getSession();
 				$session->set('block_access', true);
 	//			return true;
@@ -49,15 +50,39 @@ class plgSystemBlock_access extends JPlugin {
 		}
 	}
 
+
 	function blockArea() {
 		if($this->params->get('typeOfBlock') == "message") {
-			header('HTTP/1.0 403 Forbidden');
-			die($this->params->get('text'));
+			header('HTTP/1.0 401 Unauthorized');
+			die($this->params->get('message'));
 		}
-		elseif($this->params->get('typeOfBlock') == "errorpage") {
-			$uri = Uri::getInstance();
-			$url = $uri->toString();
-			(CMSApplication::getInstance('site'))->redirect(JUri::root(), 301);
+		elseif($this->params->get('typeOfBlock') == "redirect") {
+			$current = Uri::getInstance();
+			$currentScheme = $current->getScheme();
+			$currentHost = $current->getHost();
+			$currentPath = $current->getPath();
+			$currentUrl = $current->toString();
+
+			// Make sure that there's a leading slash!
+			$redirectPath = (strpos($this->params->get('redirectUrl'),0,1) == "/") ? $this->params->get('redirectUrl') : '/'.$this->params->get('redirectUrl');
+
+			$redirect = Uri::getInstance(JUri::root());
+			$redirectScheme = $redirect->getScheme();
+			$redirectHost = $redirect->getHost();
+			$redirectPath = JUri::root(true).$redirectPath;
+			$redirectUrl = $redirect->setScheme($redirectScheme)
+															->setHost($redirectHost)
+															->setPath($redirectPath);
+			$redirectUrl = $redirect->toString();
+
+			// If the current URL is the main page, do nothing
+			if($currentUrl == $redirectUrl) return;
+
+			// Else: Redirect to given URL
+			(CMSApplication::getInstance('site'))->redirect($redirectUrl, 301);//JUri::root()
+		}
+		else{
+			(CMSApplication::getInstance('site'))->redirect($redirectUrl, 301);
 		}
 	}
 }
